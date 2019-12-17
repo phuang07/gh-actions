2c317e3906c7cb0753f7350c81e88259c41891c0
Ray Huang <leoman730@gmail.com>
2019-12-17 14:50:20 -0500

    Update script

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,14 +19,14 @@ echo "Running docker jekyll/jekyll:$1"
 
 
 docker run --rm \
-    --name "jekyll-container"
+    --name "jekyll_container"
     --volume="$PWD:/srv/jekyll" \
     -it jekyll/jekyll:$JEKYLL_VERSION \
     ls
 
 docker ps
 
-docker exec jekyll-container jekyll build
+docker exec jekyll_container jekyll build
 
 git status
 exit 0
